9c1c2df5ff7ba7dbf030dceea8d4a30c3e2baffa
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2015-06-07 22:41:31 -0700

    Modify the code to remove duplicated circles and classify users with the circle(s) they belong to.

--- visualization/prototype/generateJson.py
+++ visualization/prototype/generateJson.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import numpy as np
-import xml.etree.cElementTree as ET
 import json
 
 # The circle class used to construct the hierarchy graph of different circles in one ego network
@@ -14,7 +13,7 @@ class Circle:
     
 if __name__ == "__main__":
     path = "../../data/facebook/"
-    user_id = 107
+    user_id = 1912
     filename_circle = "{0}{1}.circles".format(path, user_id)
     
     with open(filename_circle) as f:
@@ -22,7 +21,10 @@ if __name__ == "__main__":
         
     lines_str = [line.split('\t') for line in lines_str]
     lines_num = [list(map(int, [line[0][6:]] + line[1:])) for line in lines_str]
-    circles = [Circle(idx, line[1:]) for idx, line in enumerate(lines_num)]
+    #FIXME remove identical circles
+    member_id = {tuple(line[1:]): idx for idx, line in enumerate(lines_num)}
+    
+    circles = [Circle(idx, list(member)) for member, idx in member_id.items()]
     
     n_circle = len(circles)
     flag_ancestor = np.zeros((n_circle, n_circle), dtype=np.int) # If the i-th circle is a superset of the j-th circle, then we call circle i is an ancector of j or j is an offspring of i. Correspondingly, we set flag_ancestor[i, j] = 1 and flag_ancestor[j, i] = -1. Otherwise, flag_ancestor[i, j] = flag_ancestor[j, i]  = 0
@@ -47,29 +49,44 @@ if __name__ == "__main__":
             if not(np.any(flag_ancestor[ancestor, ancestor_rest] == 1)): 
                 circles[i_circle].parents.add(circles[ancestor].id)
                 circles[ancestor].children.add(circles[i_circle].id)
-               
-    
-    # Write the circle hierarchical DAG into a xml file
-    root = ET.Element("dag")
-    vertices = ET.SubElement(root, "vertices")
-    edges = ET.SubElement(root, "edges")
-    for i_circle in range(n_circle - 1):
-        ET.SubElement(vertices, "vertex").text = str(i_circle)
-        for child in circles[i_circle].children:
-            edge = ET.SubElement(edges, "edge")
-            ET.SubElement(edge, "s").text = str(i_circle)
-            ET.SubElement(edge, "d").text = str(child)
-    tree = ET.ElementTree(root)
-    filename = "{0}.hierarchy.xml".format(user_id)
-    tree.write(filename)
-    
-    
+            
     # Write the circle hierarchical DAG into a json file
-    edges = [dict(s=i_circle, d=child) for i_circle in range(n_circle - 1) for child in circles[i_circle].children]
-    vertices = {i_circle: {"description": '{0} users: '.format(len(circles[i_circle].members)) + ', '.join(str(x) for x in circles[i_circle].members)} for i_circle in range(n_circle)}
+    edges = [dict(s=circles[i_circle].id, d=child) for i_circle in range(n_circle - 1) for child in circles[i_circle].children]
+    vertices = {circles[i_circle].id: {"description": '{0} users: '.format(len(circles[i_circle].members)) + ', '.join(str(x) for x in circles[i_circle].members)} for i_circle in range(n_circle)}
     data ={"user": user_id, "vertices": vertices, "edges": edges}
     filename = "{0}.hierarchy.json".format(user_id)
     with open(filename, 'w') as outfile:
         json.dump(data, outfile)
+        
+    # Generate the ego network
+    users = dict()
+    [Circle(idx, line[1:]) for idx, line in enumerate(lines_num)]
+    for circle in circles:
+        for user in circle.members:
+            users.setdefault(user, set()).add(circle.id)
+    print(len(users))
+            
+    filename_link = "{0}{1}.edges".format(path, user_id)
+    with open(filename_link) as f:
+        lines_str = f.read().splitlines()
+        
+    lines_str = [line.split(' ') for line in lines_str]
+    links = list({tuple(map(int, line)) for line in lines_str})
+    for link in links: # Add the rest of the users in the ego network even if they are not in any circle
+        users.setdefault(link[0], set([-1]))
+        users.setdefault(link[1], set([-1]))
+    print(len(users))
+    #print(users[2283])
+    
+    
+    circle_map = {circle.id: circle for circle in circles}
+    for user, user_circles in users.items(): # if a user is in set A and set A is a subset of set B, then we don't say this user is in set B
+        circle_to_remove = set()
+        for circle_sub in user_circles:
+            for circle_sup in user_circles:
+                if circle_sub != circle_sup and circle_sub > 0 and (circle_sup in circle_map[circle_sub].parents):
+                    circle_to_remove.add(circle_sup)
+                    
+        users[user] -= circle_to_remove
     
 
