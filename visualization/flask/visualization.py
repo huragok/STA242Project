f77112b2233f5ac182726fd140722da898b9a8bd
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2015-06-09 17:19:19 -0700

    Now the application can execute the community detection algorithm.
    Just need to modify the template a little more.

--- visualization/flask/visualization.py
+++ visualization/flask/visualization.py
@@ -1,5 +1,8 @@
 from flask import Flask, url_for
 from flask import render_template, request
+import os
+import visualization_util
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +11,33 @@ def index():
     
 @app.route('/user', methods=['POST'])
 def user():
-    return render_template('visualization.html', userID=request.form['userID']) 
+    return render_template('visualization.html', userID=request.form['userID'])
     
+@app.route('/execute', methods=['POST'])
+def execute():
+  
+    # Run the algorithm on the server, generate the json file and return
+    userID = request.form['userID']
+    K = request.form['K']
+    path = "./static/"
+    
+    filename_out = "{0}_{1}.out".format(userID, K)
+    print("{0}{1}".format(path, filename_out))
+    if not os.path.isfile("{0}{1}".format(path, filename_out)): # Execute the community detection algorithm only when the output file does not exist.
+        os.system("{0}cluster {0}facebook/{1} {2} {0}{3}".format(path, userID, K, filename_out))
+    
+    with open("{0}{1}".format(path, filename_out)) as f:
+        lines_str = f.read().splitlines()
+    
+    circles_str = lines_str[5][13 : -2].split("],[")
+    circle_members = [list(map(int, circle_str.split(','))) for circle_str in circles_str]
+    file_edge = "{0}facebook/{1}.edges".format(path, userID)
+    file_hierarchy = "{0}{1}_{2}.hierarchy.json".format(path, userID, K)
+    file_ego = "{0}{1}_{2}.ego.json".format(path, userID, K)
+
+    n = visualization_util.output_plot_json(circle_members, file_edge, file_hierarchy, file_ego, userID)
+    
+    return render_template('visualization.html', userID=request.form['userID'], K=K)
+
 if __name__ == '__main__':
     app.run(debug=True)
--- visualization/flask/visualization_util.py
+++ visualization/flask/visualization_util.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import numpy as np
 import json
 import random
@@ -11,24 +10,12 @@ class Circle:
         self.parents = set()
         self.children = set()
         self.depth = None # The depth of the set in the super set graph (a dag)
-    
-if __name__ == "__main__":
-    path = "../../data/facebook/"
-    user_id = 698
-    filename_circle = "{0}{1}.circles".format(path, user_id)
-    
-    with open(filename_circle) as f:
-        lines_str = f.read().splitlines()
-        
-    lines_str = [line.split('\t') for line in lines_str]
-    lines_num = [list(map(int, [line[0][6:]] + line[1:])) for line in lines_str]
-    #FIXME remove identical circles
-    #circles = [Circle(idx, list(line[1:])) for idx, line in enumerate(lines_num)]
-    member_id = {tuple(line[1:]): idx for idx, line in enumerate(lines_num)}
-    circles = [Circle(idx, list(member)) for member, idx in member_id.items()]
-    
+
+# The function to output hierarchy and ego json files given a list of cirles
+def output_plot_json(circle_members, file_edge, file_hierarchy, file_ego, user_id):
+    circles = [Circle(idx, list(member)) for idx, member in enumerate(circle_members)]
     n_circle = len(circles)
-    flag_ancestor = np.zeros((n_circle, n_circle), dtype=np.int) # If the i-th circle is a superset of the j-th circle, then we call circle i is an ancector of j or j is an offspring of i. Correspondingly, we set flag_ancestor[i, j] = 1 and flag_ancestor[j, i] = -1. Otherwise, flag_ancestor[i, j] = flag_ancestor[j, i]  = 0
+    flag_ancestor = np.zeros((n_circle, n_circle), dtype=np.int)
     list_ancestor = [list() for i_circle in range(n_circle)]
     for i_circle in range(n_circle - 1):
         for j_circle in range(i_circle + 1, n_circle):
@@ -41,8 +28,6 @@ if __name__ == "__main__":
                 flag_ancestor[j_circle, i_circle] = 1
                 list_ancestor[i_circle].append(j_circle)
                 
-    
-    # If circle M is a superset of circle N and there is no circle that is both a subset of M and a super set of N, than we call M is a parent of N and N is a child of M. Next we figure out the parental relationship between all circles
     for i_circle in range(n_circle):
         for ancestor in list_ancestor[i_circle]:
             # Check all other ancestors of i_circle
@@ -58,8 +43,7 @@ if __name__ == "__main__":
     edges = [dict(s=circles[i_circle].id, d=child) for i_circle in range(n_circle - 1) for child in circles[i_circle].children]
     vertices = {circles[i_circle].id: {"description": '{0} users: '.format(len(circles[i_circle].members)) + ', '.join(str(x) for x in circles[i_circle].members), "group": group_map[circles[i_circle].id]} for i_circle in range(n_circle)}
     data ={"user": user_id, "vertices": vertices, "edges": edges}
-    filename = "{0}.hierarchy.json".format(user_id)
-    with open(filename, 'w') as outfile:
+    with open(file_hierarchy, 'w') as outfile:
         json.dump(data, outfile)
         
     # Generate the ego network
@@ -67,47 +51,22 @@ if __name__ == "__main__":
     for circle in circles:
         for user in circle.members:
             users.setdefault(user, set()).add(circle.id)
-    print(len(users))
-            
-    filename_link = "{0}{1}.edges".format(path, user_id)
-    with open(filename_link) as f:
+    
+    with open(file_edge) as f:
         lines_str = f.read().splitlines()
-        
+    
     lines_str = [line.split(' ') for line in lines_str]
     links_raw = list({tuple(map(int, line)) for line in lines_str})
     for link in links_raw: # Add the rest of the users in the ego network even if they are not in any circle
         users.setdefault(link[0], set([-1,]))
         users.setdefault(link[1], set([-1,]))
-    print(len(users))
-    #print(users[2283])
-    
-    
-    
-    #for user, user_circles in users.items(): # if a user is in set A and set A is a subset of set B, then we don't say this user is in set B
-    #    circle_to_remove = set()
-    #    for circle_sub in user_circles:
-    #        for circle_sup in user_circles:
-    #            if circle_sub != circle_sup and circle_sub > 0 and (circle_sup in circle_map[circle_sub].parents):
-    #                circle_to_remove.add(circle_sup)
-                    
-    #    users[user] -= circle_to_remove
-        
-    # Convert the circles that a user belong to into a tuple and count the number of circles it belong to
-    users = {key: tuple(value) for key, value in users.items()}
-    
-    
-    
+
+    users = {key: tuple(value) for key, value in users.items()}    
     nodes = [dict(name = key, group = group_map[random.choice(value)], circles = [circle for circle in value if circle != -1], groups = [group_map[circle] for circle in value]) for key, value in users.items()]
     
     user_map = {value: key for key, value in enumerate(users.keys())}
     links = [dict(source = user_map[link[0]], target = user_map[link[1]], value = 1) for link in links_raw]
-    
-    filename = "{0}.ego.json".format(user_id)
-    with open(filename, 'w') as outfile:
+    with open(file_ego, 'w') as outfile:
         json.dump({"nodes": nodes, "links": links, "ncircle": n_circle, "nuser": len(users)}, outfile, indent=2)
-    
         
-   
- 
-    
-
+    return (n_circle)
